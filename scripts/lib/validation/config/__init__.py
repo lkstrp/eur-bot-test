776a1e48cbceb8a7cdf27134bdca01194dd9ee9b
lkstrp <lkstrp@pm.me>
2026-02-24 17:23:34 +0100

    trigger

--- scripts/lib/validation/config/__init__.py
+++ scripts/lib/validation/config/__init__.py
@@ -58,7 +58,7 @@ class LoggingConfig(ConfigModel):
         description="Restrict console outputs to all infos, warning or errors only",
     )
     format: str = Field(
-        "%(levelnameasdasdaasdasdsdsa)sasd:asdasdasd%(name)s:%(message)s",
+        "%(asd)sasd:asdasdasd%(name)s:%(message)s",
         description="Custom format for log messages. See `LogRecord <https://docs.python.org/3/library/logging.html#logging.LogRecord>`_ attributes.",
     )
 
